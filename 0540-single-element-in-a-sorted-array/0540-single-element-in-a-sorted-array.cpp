3d90aa799c27a3385c37bbe16e68fbd2193b78b4
Tushar Singh bhati <Bhatikrishna447@gmail.com>
2026-08-12 20:22:04 +0530

    Time: 0 ms (100%), Space: 26.3 MB (36.1%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -1,33 +1,32 @@
 class Solution {
 public:
-    int singleNonDuplicate(vector<int>& n) {
-        int str=0, end=n.size()-1;
-        if(n.size()==1){
-            return n[0];
+    int singleNonDuplicate(vector<int>& nums) {
+        int str=0, end=nums.size()-1;
+        if(nums.size()==1){
+            return nums[0];
         }
         while(str<=end){
             int mid=str+(end-str)/2;
-            if(mid==0 && n[mid]!=n[1]){
-                return n[mid];
+            if(mid==0 && nums[mid]!=nums[mid+1]){
+                return nums[mid];
+            }if(mid==nums.size()-1 && nums[mid-2]!=nums[mid-1]){
+                return nums[mid];
             }
-            if(mid==n.size()-1 && n[mid]!=n[mid-2]){
-                return n[mid];
-            }
-            if(n[mid-1]!=n[mid] && n[mid]!=n[mid+1]){
-                return n[mid];
+            if(nums[mid-1]!=nums[mid] && nums[mid]!=nums[mid+1]){
+                return nums[mid];
             }
             if(mid%2==0){
-                if(n[mid-1]==n[mid]){
+                if(nums[mid-1]==nums[mid]){
                     end=mid-1;
-                }
-                else{
-                    str=mid+1;
-                }
-            }else{
-                if(n[mid-1]==n[mid]){
-                    str=mid+1;
                 }else{
+                    str=mid+1;
+                }
+            }
+            else{
+                if(nums[mid+1]==nums[mid]){
                     end=mid-1;
+                }else{
+                    str=mid+1;
                 }
             }
         }
