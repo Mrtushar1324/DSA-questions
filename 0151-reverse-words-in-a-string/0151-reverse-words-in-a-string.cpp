399381bcd5d6735dd04aa1d4709cc20c9eece32f
Tushar Singh bhati <Bhatikrishna447@gmail.com>
2026-08-30 22:59:39 +0530

    Time: 1 ms (45.43%), Space: 10.2 MB (62.63%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
@@ -3,13 +3,16 @@ public:
     string reverseWords(string s) {
         int n=s.length();
         string ans="";
+        
         reverse(s.begin(), s.end());
+
         for(int i=0; i<n; i++){
             string word="";
             while(i<n && s[i] != ' '){
                 word+=s[i];
                 i++;
             }
+            
             reverse(word.begin(), word.end());
             if(word.length()>0){
                 ans+=" "+word;
