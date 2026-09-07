cbe091a598a4b3ecfbb3deef6856ad9d8b6b603b
Tushar Singh bhati <Bhatikrishna447@gmail.com>
2026-09-07 23:01:02 +0530

    Time: 243 ms (9.04%), Space: 9.7 MB (98.75%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.cpp
+++ 0567-permutation-in-string/0567-permutation-in-string.cpp
@@ -1,8 +1,8 @@
 class Solution {
 public:
-    bool issame(int freq1[], int freq2[]){
-        for(int i=0;i<26;i++){
-            if(freq1[i]!= freq2[i]){
+    bool issame(int b1[], int b2[]){
+        for(int i=0; i<26;i++){
+            if(b1[i]!=b2[i]){
                 return false;
             }
         }
@@ -13,15 +13,15 @@ public:
         for(int i=0; i<s1.length(); i++){
             freq[s1[i]-'a']++;
         }
-        for(int i=0; i<s2.length(); i++){
-            int idx=i , windidx=0;
+        int windsize=s2.length();
+        for(int i=0; i<s2.length();i++){
+            int idx=i, windidx=0;
             int windfreq[26]={0};
             while(windidx<s1.length() && idx<s2.length()){
                 windfreq[s2[idx]-'a']++;
                 idx++;
                 windidx++;
             }
-            
             if(issame(freq, windfreq)){
                 return true;
             }
