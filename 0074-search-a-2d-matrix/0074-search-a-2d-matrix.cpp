19f91ca366dcecc19fc5081fe8f272cd047c5ebf
Tushar Singh bhati <Bhatikrishna447@gmail.com>
2026-09-11 14:35:56 +0530

    Time: 0 ms (100%), Space: 13.4 MB (6.45%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -1,34 +1,34 @@
 class Solution {
 public:
-    bool Inrow(vector<vector<int>>&mat , int tar, int row){
+    bool isfound(vector<vector<int>>& mat, int midrow, int tar){
         int n=mat[0].size();
-        int str=0, end=n-1;
+        int str =0, end =n-1;
         while(str<=end){
-            int mid=str+(end-str)/2;
-            if(tar==mat[row][mid]){
+            int mid1=str+(end-str)/2;
+            if(tar==mat[midrow][mid1]){
                 return true;
             }
-            else if(tar>mat[row][mid]){
-                str=mid+1;
+            else if(tar> mat[midrow][mid1]){
+                str=mid1+1;
             }else{
-                end=mid-1;
+                end=mid1-1;
             }
         }
         return false;
     }
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
         int m=matrix.size(), n=matrix[0].size();
-        int strtrow=0, endrow=m-1;
-        while(strtrow<=endrow){
-            int midrow=strtrow+(endrow-strtrow)/2;
-            if(target>=matrix[midrow][0] && target<=matrix[midrow][n-1]){
-                return Inrow(matrix, target , midrow);
+        int str=0, end=m-1;
+        while(str<=end){
+            int mid=str+(end-str)/2;
+            if(target>=matrix[mid][0] && target<=matrix[mid][n-1]){
+                return isfound(matrix , mid , target);
             }
-            else if(target>matrix[midrow][n-1]){
-                strtrow=midrow+1;
+            else if(target>matrix[mid][n-1]){
+                str=mid+1;
             }
             else{
-                endrow=midrow-1;
+                end=mid-1;
             }
         }
         return false;
